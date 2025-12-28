3e028f14c90f18c8327abd9d8b4054fd35be1092
Dong Ky <dong.kyh@one-line.com>
2025-12-28 11:26:04 +0700

    first commit 18

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
           <iframe
             width="560"
             height="315"
-            src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+            src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&mute=0"
             title="70s Vibes"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
